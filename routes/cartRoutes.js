300abdf3ccd5d54bb27a3da80885e133a20a0410
gia.c.catimbang <gia.c.catimbang@gmail.com>
2025-04-07 19:57:35 +0800

    Updated s53

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -10,13 +10,13 @@ const router = express.Router();
 router.get("/active", verify, cartController.getActiveCart);
 
 // Add to Cart
-router.post("/add-to-cart", verify, cartController.addToCart);
+router.post("/active/add-to-cart", verify, cartController.addToCart);
 
 // Retrieve User's Cart 
 router.get("/get-cart", verify, cartController.getCart);
 
 // Update Cart Quantity
-router.put("/update-cart-quantity", verify, cartController.updateCartQuantity);
+router.put("/active/update-cart-quantity", verify, cartController.updateCartQuantity);
 
 module.exports = router;
 
