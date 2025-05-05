04a714264c3c35ab799f50bf9f90a303dd6abecc
gia.c.catimbang <gia.c.catimbang@gmail.com>
2025-05-05 20:48:40 +0800

    Updated for cps3

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ require('dotenv').config();
 // Creates an "app" variable that stores the result of the "express" function that initializes our express application and allows us access to different methods that will make backend creation easy
 const app = express();
 const corsOptions = {
-	origin: ['http://localhost:8000'],
+	origin: ['http://localhost:3000'],
 	credentials: true,
 	optionsSuccessStatus: 200
 };
